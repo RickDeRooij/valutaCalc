20eb34aabfe74699ea2861f60a2d024f422f3e7e
rickderooij <rick.derooij@ictmbo.nl>
2019-03-18 14:23:41 +0100

    git

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,6 +1,11 @@
- let b = document.getElementById('button').addEventListener("click", function(){
-    let a = document.getElementById('input').value;
-    dollar.innerHTML = a * 1.1346 ;
-    pond.innerHTML = a * 0.8708;
-    kronen.innerHTML = a * 7.4608;
-});
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+    console.log('hoi2');
+    let b = document.getElementById('button').addEventListener("click", function(){
+        let a = document.getElementById('input').value;
+        dollar.innerHTML = a * 1.1346 ;
+        pond.innerHTML = a * 0.8708;
+        kronen.innerHTML = a * 7.4608;
+    });
+}
